9aabc78081cae57c17d1a335d7fa0676749f3641
Sergie Gomez <mlsergie123@gmail.com>
2026-05-12 03:14:19 +0800

    update

--- testbench.v
+++ testbench.v
@@ -1,21 +1,23 @@
 // testbench for seq_0110
 module testbench;
   // tb signals
-  reg in;
-  reg rst_n;
-  reg clk_in;
-  wire clk_led;
-  wire out;
-  wire [0:6] hex;
+reg        ovr;
+reg        rst_n;
+reg       clk_in;
+wire       clk_led;
+wire       out;
+wire [0:6] hex_timer;
+wire [0:6] hex_state;
 
   // instantiate top level module
-  top_seq_0110 seqnon(
+  top_seq_timer seqnon(
     .out(out), 
-    .hex(hex), 
+    .hex_timer(hex_timer), 
     .clk_led(clk_led), 
     .clk_in(clk_in),
     .rst_n(rst_n), 
-    .in(in)
+    .hex_state(hex_state),
+    .ovr(ovr)
   );
  	
   initial clk_in = 0;
@@ -23,15 +25,15 @@ module testbench;
 
   // apply stimuli
 initial begin 
-  rst_n = 0; in = 0; #12;
-  rst_n = 1; in = 0; #4;
-  rst_n = 1; in = 1; #4;  
-  rst_n = 1; in = 1; #4; 
-  rst_n = 1; in = 0; #4;
-  rst_n = 1; in = 0; #4;
-  rst_n = 1; in = 1; #4;
-  rst_n = 1; in = 1; #4;
-  rst_n = 1; in = 0; #4;  
+  rst_n = 0; ovr = 0; #12;
+  rst_n = 1; ovr = 0; #4;
+  rst_n = 1; ovr = 0; #4;  
+  rst_n = 1; ovr = 0; #4; 
+  rst_n = 1; ovr = 0; #4;
+  rst_n = 1; ovr = 0; #4;
+  rst_n = 1; ovr = 1; #4;
+  rst_n = 1; ovr = 1; #4;
+  rst_n = 1; ovr = 0; #4;  
   
   
  end
--- timer_seq.v
+++ timer_seq.v
@@ -3,9 +3,7 @@
 ====================================
 Description:
 
-
 Design Engineer:
-
 Gomez,Sergie D.
 
 Date:
@@ -16,69 +14,82 @@ module timer_seq(out,state,clk,rst_n,ovr,timer);
 input            clk;
 input            rst_n;
 input            ovr;
-output reg [3:0] timer;
+output     [3:0] timer;
 output reg       out;
 output reg [3:0] state;
-
 
 // state assignment
 parameter [3:0] S0 = 4'b0000;
 parameter [3:0] S1 = 4'b0001;
 parameter [3:0] S2 = 4'b0010;
 parameter [3:0] S3 = 4'b0011;
-reg [3:0] nxt;//next state
-reg [3:0] pre;//present state
-reg [3:0] t=0;
-
+reg [3:0] nxt_state; // next state
+reg [3:0] pre_state; // present state
+reg [3:0] t = 4'b0;  // timer counter
 
 // input block
-always @(t,pre)begin
- case (pre)
-   S0: nxt = (t==2)? S1:S0;
-   S1: nxt = (t==3)? S2:S1;
-   S2: nxt = (t==2)? S0:S2;
-   S3: nxt = (t==1)? S3:S0;
-   
+always @(t,pre_state,ovr) begin
+    if (ovr) begin
+        nxt_state = S3;
+    end else begin
+        case (pre_state)
+            S0: nxt_state = (t==4'd2)? S1:S0;
+            S1: nxt_state = (t==4'd3)? S2:S1;
+            S2: nxt_state = (t==4'd2)? S0:S2;
+            S3: nxt_state = (ovr==0)? S0:S3;
+            default: nxt_state = S0;
+        endcase
+    end
+end
 
-   
-   default: nxt = S0;
-	endcase
-  end
- 
- 
 // sequential block
- always @(posedge clk,negedge rst_n)begin
-if(!rst_n)begin
-t = 0;
-ovr = 0;
-end
-	else pre<=nxt;
-	if(pre==S0)begin
-		if(t==2)begin
-			nxt<=S1;
-			t<=0;
-		end
-		else t=t+1;
-	end
-	else if(pre==S1)begin
-		if(t==3) begin 
-			nxt<=S2;
-			t<=0;
-		end
-		else t=t+1;
-	end
-	else if(pre==S2)begin
-		if(t==2)begin
-			nxt<=S0;
-			t<=0;
-		end
-		else t=t+1;
-	end
-	else if(ovr)begin
-	nxt<=S3;
-	t<=0;
-	end
+always @(posedge clk, negedge rst_n) begin
+    if(!rst_n) begin
+        pre_state <= S0;
+        t         <= 4'b0;
+    end
+    else if(ovr) begin
+        pre_state <= nxt_state;
+        t         <= 4'b0;                          
+    end
+    else begin
+        pre_state <= nxt_state;
+        // state_timer
+        case(pre_state)
+            S0: t <= (t==4'd2)? (4'b0):(t + 4'b1);  
+            S1: t <= (t==4'd3)? (4'b0):(t + 4'b1);  
+            S2: t <= (t==4'd2)? (4'b0):(t + 4'b1);  
+            S3: t <= 4'b0;                           
+        endcase
+    end
 end
 
+// output block
+always @(pre_state) begin
+    case(pre_state)
+        S0: begin
+                out   = 0;
+                state = S0;
+            end
+        S1: begin
+                out   = 1;
+                state = S1;
+            end
+        S2: begin
+                out   = 0;
+                state = S2;
+            end
+        S3: begin
+                out   = 1;
+                state = S3;
+            end
+        default: begin
+                out   = 0;
+                state = S0;
+            end
+    endcase
+end
+
+assign timer = t;
 
 endmodule
